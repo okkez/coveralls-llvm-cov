3fa06d0801f77901ddd6617ae2a2fcfc8cb74cbf
okkez <okkez000@gmail.com>
2018-03-21 17:56:53 +0900

    Create new gem

---
+++ lib/coveralls/llvm/cov.rb
@@ -0,0 +1,8 @@
+require "coveralls/llvm/cov/version"
+
+module Coveralls
+  module LLVM
+    module Cov
+    end
+  end
+end
---
+++ lib/coveralls/llvm/cov/version.rb
@@ -0,0 +1,7 @@
+module Coveralls
+  module LLVM
+    module Cov
+      VERSION = "0.1.0"
+    end
+  end
+end
